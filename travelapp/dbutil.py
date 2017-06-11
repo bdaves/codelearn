4beaa45641728953c5a982232a2bfb2d612f485b
Stephen Davis <stdavi@amazon.com>
2017-06-11 16:15:07 -0400

    Update dbutil to match new database schema

--- travelapp/dbutil.py
+++ travelapp/dbutil.py
@@ -11,22 +11,34 @@ def connect():
     return pymysql.connect(host=cfg.DB_HOST, user=cfg.DB_USERNAME, 
                            passwd=cfg.DB_PASSWORD, db=cfg.DB_DATABASE)
 
+def get_guid():
+    return uuid.uuid1().hex
 
-
-def insert_location(cursor, userid, title, latitude, longitude, arrivalDate, departureDate, website):
-    sql = """
-        INSERT INTO locations (user_id, guid, title, latitude, longitude, arrivalDate, departureDate, url)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
-    """
-
-    guid = uuid.uuid1().hex
-
-    cursor.execute(sql, (userid, guid, title, latitude, longitude, arrivalDate, departureDate, website))
-
-    cursor.connection.commit()
+def generate_salt():
+    return ''.join(random.sample(CHAR_SET * 64, 64))
 
 def hash_password(salt, password):
     return hashlib.sha256((salt + password).encode('utf-8')).hexdigest()
+
+
+def insert_location(cursor, tripid, title, latitude, longitude, arrivalDate, departureDate, website):
+    sql = """
+        INSERT INTO locations (trip_id, guid, title, latitude, longitude, arrivalDate, departureDate, url)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """
+
+    cursor.execute(sql, (tripid, get_guid(), title, latitude, longitude, arrivalDate, departureDate, website))
+    cursor.connection.commit()
+
+def insert_trip(cursor, group_id, title):
+    sql = """
+        INSERT INTO trips (group_id, guid, title)
+        VALUES (%s, %s, %s)
+    """
+
+    cursor.execute(sql, (group_id, get_guid(), title))
+    cursor.connection.commit()
+
 
 def validate_user(cursor, username, password):
     user = get_user(cursor, username)
@@ -40,20 +52,20 @@ def validate_user(cursor, username, password):
 
     return expected_hash == hashed_pw
 
+
 def insert_user(cursor, username, firstname, lastname, email, password):
     sql = """
         INSERT INTO users (guid, username, firstname, lastname, email, hashed_password, salt)
-        VALUES (%s, %s, %s, %s, %s, %s, %s);
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
     """
-    salt = ''.join(random.sample(CHAR_SET*64, 64))
+    salt = generate_salt()
 
     hashed_password = hash_password(salt, password)
 
-    guid = uuid.uuid1().hex
-
-    cursor.execute(sql, (guid, username, firstname, lastname, email, hashed_password, salt))
+    cursor.execute(sql, (get_guid(), username, firstname, lastname, email, hashed_password, salt))
 
     cursor.connection.commit()
+
 
 def get_user(cursor, username):
     sql = """
@@ -84,7 +96,7 @@ def get_user(cursor, username):
 
 def get_locations(cursor, username):
     sql = """
-        SELECT locations.location_id, locations.user_id, locations.guid, locations.title,
+        SELECT locations.location_id, locations.trip_id, locations.guid, locations.title,
                locations.latitude, locations.longitude, locations.arrivalDate, 
                locations.departureDate, locations.url
         FROM locations
@@ -99,7 +111,7 @@ def get_locations(cursor, username):
     while (location != None):
         location_list.append({
             "location_id": location[0],
-            "user_id": location[1],
+            "trip_id": location[1],
             "guid": location[2],
             "title": location[3],
             "latitude": location[4],
